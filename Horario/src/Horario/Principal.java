558c21a43a884474a7c6ba827ab83bec0ba2c4ac
Raul Ruiz <ruiz.carau25@triana.salesianos.edu>
2026-01-09 13:55:07 +0100

    Añadido case 3

--- Horario/src/Horario/Principal.java
+++ Horario/src/Horario/Principal.java
@@ -6,13 +6,14 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		int op;
+		int op, dia;
 		
 		System.out.println("Bienvenido a tu horario personal");
 		do {
 			System.out.println("Elija una opcion");
 			System.out.println("Pulse 1.- Ver horario por día");
 			System.out.println("Pulse 2.- Ver horas de casa asignatura a la semana");
+			System.out.println("Pulse 4.- Ver el horario de cada día");
 			op=Leer.datoInt();
 			
 			switch (op) {
@@ -34,6 +35,41 @@ public class Principal {
 					System.out.println("FOP \t\t1 hora");
 					System.out.println("Digitalización \t1 hora");
 					System.out.println("Sostenibilidad \t1 hora");
+					break;
+					
+				case 4:
+					System.out.println("Seleccione que dia quiere ver el horario");
+					System.out.println("1.-Lunes");
+					System.out.println("2.-Martes");
+					System.out.println("3.-Miercoles");
+					System.out.println("4.-Jueves");
+					System.out.println("5.-Viernes");
+					dia=Leer.datoInt();
+					switch (dia) {
+					case 1:
+						System.out.println("Tus asignaturas del lunes son :1ºSASP, 2ºFOP, 3ºBBDD, 4ºPROG, 5ºPROG, 6ºEEDD");
+						break;
+						
+					case 2:
+						System.out.println("Tus asignaturas del martes son :1ºLMSGI, 2ºPROG, 3ºIPE1, 4ºSI, 5ºBBDD, 6ºSI");
+						break;
+						
+					case 3:
+						System.out.println("Tus asignaturas del miercoles son :1ºLMSGI, 2ºIPE1, 3ºPROG, 4ºPROG, 5ºSI, 6ºBBDD");
+						break;
+						
+					case 4:
+						System.out.println("Tus asignaturas del jueves son :1ºIPE1, 2ºPROG, 3ºEEDD, 4ºSI, 5ºBBDD, 6ºSI");
+						break;
+						
+					case 5: 
+						System.out.println("Tus asignaturas del viernes son :1ºDASP, 2ºLMSGI, 3ºBBDD, 4ºPROG, 5ºPROG, 6ºEEDD");
+						break;
+
+					default:
+						System.out.println("Entrada de datos incorrecta");
+						break;
+					}
 					break;
 
 				default:
